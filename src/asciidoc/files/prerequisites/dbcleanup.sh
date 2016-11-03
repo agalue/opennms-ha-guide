ec712dffda886fa453a4acb5ae6cd98a3f9ad2aa
Alejandro Galue <agalue@opennms.org>
2016-11-03 11:10:47 -0400

    Improving pre-requisites.

--- src/asciidoc/files/prerequisites/dbcleanup.sh
+++ src/asciidoc/files/prerequisites/dbcleanup.sh
@@ -11,5 +11,5 @@ echo
 rm -f /tmp/.s.PGSQL.9* /var/log/pgpool-II-95/*
 for pgserver in $DB_SRVS; do
 echo "Cleaning up DB connections on $pgserver ..."
-su - postgres -c "/usr/bin/ssh -T -l postgres $pgserver '~/9.5/cleanup.sh $ONMS_DB $ONMS_SRV'"
+su - postgres -c "/usr/bin/ssh -T -l postgres $pgserver '/usr/loca/bin/dbcleanup.sh $ONMS_DB $ONMS_SRV'"
 done
