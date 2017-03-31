ad9110b5932b21c4ec022f0dd01b6a7702cbc863
Alejandro Galue <agalue@opennms.org>
2017-03-31 11:49:39 -0400

    Applying several updates...
    * Archive mode is not required when using streaming replication.
    * Improving default pgpool settings (health check).
    * Updating Grafana on the Vagrant scripts.
    * Minor format enhancements.

--- src/vagrant/files/opennms.sh
+++ src/vagrant/files/opennms.sh
@@ -31,7 +31,7 @@ fi
 if ! rpm -qa | grep -q grafana; then
   echo "Installing Grafana ..."
   sudo yum install -y initscripts fontconfig
-  sudo yum install -y https://grafanarel.s3.amazonaws.com/builds/grafana-3.1.1-1470047149.x86_64.rpm
+  sudo yum install -y https://grafanarel.s3.amazonaws.com/builds/grafana-4.1.1-1484211277.x86_64.rpm
   sudo grafana-cli plugins install opennms-datasource
 fi
 
